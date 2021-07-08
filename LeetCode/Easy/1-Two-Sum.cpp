32f48f24e6106aeae2b39b50c0ce77073534bf9a
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-07-08 16:35:15 +0530

    Update 1-Two-Sum.cpp

--- LeetCode/Easy/1-Two-Sum.cpp
+++ LeetCode/Easy/1-Two-Sum.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> sum2;
-        for(int i=0;i<nums.size();i++){
-            for(int j=1;j<nums.size();j++){
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j] == target){
                     sum2.push_back(i);
                     sum2.push_back(j);

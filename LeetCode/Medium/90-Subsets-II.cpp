b03b5013a821f6d63be709d457b77c85f5925d15
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-03 22:50:41 +0530

    Update 90-Subsets-II.cpp

--- LeetCode/Medium/90-Subsets-II.cpp
+++ LeetCode/Medium/90-Subsets-II.cpp
@@ -4,7 +4,7 @@
 // Space Complexity : O(2^n)
 // Concept Used : Recursive Tree
 
-//Approach : If all elemts are unique/duplicats allowed then we will get 2^n subsets. But in this problem we have to remove duplicate.
+//Approach : If all elemts are unique or duplicats-allowed then we will get 2^n subsets. But in this problem we have to remove duplicate.
 //            So first we will sort the given vector to identify duplicates easily. Then we will create two vectors one multidimentional for storing the subsets(ans) and second linear vector for storing the elements of each subset 
 //            Now, let's talk about out recursive function findSubsets :-
 //            We will 

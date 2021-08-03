f83b6c1f7e329340ec1327da8dc18094b90f18e2
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-03 23:17:42 +0530

    Update 90-Subsets-II.cpp

--- LeetCode/Medium/90-Subsets-II.cpp
+++ LeetCode/Medium/90-Subsets-II.cpp
@@ -5,9 +5,12 @@
 // Concept Used : Recursive Tree
 
 //Approach : If all elemts are unique or duplicats-allowed then we will get 2^n subsets. But in this problem we have to remove duplicate.
-//            So first we will sort the given vector to identify duplicates easily. Then we will create two vectors one multidimentional for storing the subsets(ans) and second linear vector for storing the elements of each subset 
-//            Now, let's talk about out recursive function findSubsets :-
-//            We will 
+//            So first we will sort the given vector to identify duplicates easily. Then we will create two vectors one multidimentional for storing the subsets(ans) and second linear vector(ds) for storing the elements of each subset 
+//            Now, let's talk about our recursive function findSubsets :-
+//            In First Statement we will store the value of subset after each call of findSubset() function. The initial value is []
+//            then, iterate loop for each branch of a recursive tree, if you find any duplicate(i.e. nums[i]==nums[i+1]) then skip for that value and dmove forward.
+//            At the end, return ans.
+
 class Solution {
 private:
     void findSubsets(int idx, vector<int> &ds, vector<vector<int>> &ans, vector<int> &nums)

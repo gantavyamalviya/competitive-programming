a9e179110228da206a1a7d498fab573a7a61f18c
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-05 00:08:33 +0530

    Update 503-Next-Greater-Element-II.cpp

--- LeetCode/Medium/503-Next-Greater-Element-II.cpp
+++ LeetCode/Medium/503-Next-Greater-Element-II.cpp
@@ -3,9 +3,9 @@
 // Space Complexity : O(n)
 // Concept used : Stack
 
-// Approach : Using brute-force approach we will get O(n^2) Time complxity and we might get TLE in some cases. So, to solve this problem efficientl we will use stack(LIFO) to reduce the Time complexity to O(n)
-//            According to question description, we can check the next greater element circularly so, to make this problem easier we will iterate till 2*n-1. 
-//            we will start from the end of given vector and iterate in reverse direction.
+// Approach : Using brute-force approach we will get O(n^2) Time complxity and we might get TLE in some cases. So, to solve this problem efficiently we will use stack(LIFO) to reduce the Time complexity to O(n)
+//            According to question description, we can check the next greater element circularly. so, to make this problem easier we will iterate till 2*n-1. 
+//            we will start from the end of given vector and iterate in reverse direction. We will use i%n to avoid memory overflow.
 //                        
 
 class Solution {

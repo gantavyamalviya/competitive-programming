b878ec26326a4c3abf9eedb63b6e9382013ae1d4
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-05 13:11:51 +0530

    Update 113-Path-Sum-II.cpp

--- LeetCode/Medium/113-Path-Sum-II.cpp
+++ LeetCode/Medium/113-Path-Sum-II.cpp
@@ -1,9 +1,10 @@
 // Question link : https://leetcode.com/problems/path-sum-ii/
-// Time Complexity :
-// Space Complexity :
-// Concept Used : Reursion, Depth-First Search, backtracking, Binary Tree
+// Time Complexity : O(n^2)
+// Space Complexity : O(H) where H is the height of tree
+// Concept Used : Recursion, Depth-First Search, backtracking, Binary Tree
 
-// approach : It is the easiest emplement of DFS algorithm. Iterate to each node and Check sum of each leaf. If the the sum is equal to the targetSum then stor it in ans vector. 
+// approach : It is the easiest emplement of DFS algorithm. Iterate to each node and Check sum of each leaf. If the the sum is equal to the targetSum then store it in ans vector.
+//            Check for each vaild path and return ans
 
 /**
  * Definition for a binary tree node.
@@ -16,22 +17,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<vector<int>> ans;
      void findpath(TreeNode* root, vector<int> &path,int targetSum, int currsum)
     {
-        if(!root){
+        if(root == NULL){
             return;
         }
         currsum+=root->val;
         path.push_back(root->val);
-        if(currsum == targetSum and !root->left and !root->right){
+        if(currsum == targetSum and !root->left and !root->right){ // found valid path
             ans.push_back(path);
         }
         findpath(root->left,path, targetSum, currsum);
         findpath(root->right,path, targetSum, currsum);
-        path.pop_back();
+        path.pop_back(); // backtracking
     }
     
     

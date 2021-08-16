ea8b969d4b06ad2c582641ee8d8ab74c3ca6023d
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-16 14:20:43 +0530

    Create 43-Multiply-Strings.cpp

---
+++ LeetCode/Medium/43-Multiply-Strings.cpp
@@ -0,0 +1,30 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(n)
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1 == "0" or num2 == "0") return "0"; // edge case
+        
+        vector<int> num(num1.size() + num2.size(), 0);
+        
+        for(int i=num1.size()-1;i>=0;i--)
+            for(int j=num2.size()-1;j>=0;j--)
+            {
+                num[i+j+1] += (num1[i] - '0') * (num2[j] - '0');
+                num[i+j] += num[i+j+1] / 10;
+                num[i+j+1] %= 10;
+            }
+        
+        // skip 0's
+        int i=0;
+        while(i < num.size() and num[i] == 0)
+            ++i;
+        
+        string ans="";
+        while(i < num.size())
+            ans.push_back(num[i++] + '0');
+        
+        return ans;
+    }
+};

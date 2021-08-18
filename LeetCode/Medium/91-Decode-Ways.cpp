089cdc4f212466a4a50804b667dc7014b036cd69
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-18 22:03:55 +0530

    Create 91-Decode-Ways.cpp

---
+++ LeetCode/Medium/91-Decode-Ways.cpp
@@ -0,0 +1,18 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+// Concept Used : Dynamic Programming
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int p = 1, pp, n = s.size();
+        for(int i=n-1;i>=0;i--) {
+            int cur = s[i]=='0' ? 0 : p;
+            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
+            pp = p;
+            p = cur;
+        }
+        return s.empty()? 0 : p;   
+    }
+};

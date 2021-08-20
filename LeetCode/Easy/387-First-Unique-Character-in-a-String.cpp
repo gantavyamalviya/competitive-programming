dce49186799a1f14473d6ee09166ca3fe7c117d1
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-20 20:16:28 +0530

    Create 387-First-Unique-Character-in-a-String.cpp

---
+++ LeetCode/Easy/387-First-Unique-Character-in-a-String.cpp
@@ -0,0 +1,20 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Concept Used : Hashing
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> m;
+        for(int i=0;i<s.size();i++)
+        {
+                m[s[i]]++;
+        }
+        for (int i = 0; i<s.size(); i++){
+            if(m[s[i]]==1){
+                return i;
+            }
+        }
+        return -1; 
+    }
+};

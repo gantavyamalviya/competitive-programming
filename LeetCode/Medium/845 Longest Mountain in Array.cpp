2b412c4642cbe8d7a06433942a0c077e6f071d49
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2021-08-25 16:42:11 +0530

    Create 845 Longest Mountain in Array.cpp

---
+++ LeetCode/Medium/845 Longest Mountain in Array.cpp
@@ -0,0 +1,21 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        if(arr.size()<3)
+            return 0;
+        int ans=0, up=0, down=0;
+        for(int i=1;i<arr.size();i++)
+        {
+            if( down and arr[i-1] < arr[i] or arr[i-1] == arr[i])
+                up=down=0;
+            up += arr[i-1] < arr[i];
+            down += arr[i-1] > arr[i];
+            if(up and down)
+                ans = max(ans, up+down+1);
+        }
+        return ans;
+    }
+};

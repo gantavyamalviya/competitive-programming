f15ae9703495c17cc264d3d774cfcc6c01c5909f
gantavya123 <91744627+gantavya123@users.noreply.github.com>
2021-10-01 20:21:57 +0530

    Create 118-Pascal's-Triangle.cpp

---
+++ LeetCode/Easy/118-Pascal's-Triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+        //ans[0][0]=1;
+        for(int i=0;i<numRows;i++){
+            ans[i].resize(i+1);
+            ans[i][0]=ans[i][i]=1;
+            
+            for(int j=1;j<i;j++){
+                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+            }
+        }
+        return ans;
+    }
+};
